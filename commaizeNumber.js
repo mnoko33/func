062d584d4865e2d34139952a3601e131ded3cfb5
sangtaek lee <srs99125@naver.com>
2020-08-02 11:42:22 +0900

    화폐 세 자리마다 , 찍어주는 함수

---
+++ commaizeNumber.js
@@ -0,0 +1,19 @@
+/* 
+    매개변수로 화폐(num)이 들어왔을 때,
+    3자리마다 ,를 찍어서 반환하는 함수
+    ex) 1000000 => 1,000,000
+*/
+
+function commaizeNumber(num) {
+    let result = '';
+    num = String(num);
+    const N = num.length;
+    
+    for (let i = 0; i < N; i++) {
+        result += num[i];
+        if ((N - i - 1) % 3 === 0 && i !== N - 1) {
+            result += ',';
+        }
+    }
+    return result;
+}
