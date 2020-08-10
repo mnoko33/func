806867024658403bc3c456dfbc0289700dd0f74e
sangtaek lee <srs99125@naver.com>
2020-08-10 20:39:17 +0900

    compareObj 함수 수정
    1. typeof와 Object.entries를 활용

--- compareObj.js
+++ compareObj.js
@@ -1,33 +1,17 @@
 // 두 개의 Object가 갖은지 비교하는 함수
 
-function compareObj(obj1, obj2) {
-    function getType(obj) {
-        return Object.prototype.toString.call(obj).slice(8, -1);
+function compareObj(x, y) {
+    const typeX = typeof x;
+    const typeY = typeof y;
+    if (typeX !== typeY) return false;
+    if (typeX !== 'object') return x === y;
+    
+    const entriesX = Object.entries(x);
+    const entriesY = Object.entries(y);
+    if (entriesX.length !== entriesY.length) return false;
+    for (let i = 0; i < entriesX.length; i++) {
+        const [k, v] = entriesX[i];
+        if (!compareObj(v, y[k])) return false;
     }
-
-    if (getType(obj1) !== getType(obj2)) return false;
-
-    if (getType(obj1) === 'Array') {
-        const N = obj1.length;
-        for (let i = 0; i < N; i++) {
-            if (!compareObj(obj1[i], obj2[i])) return false;
-        }
-
-        return true;
-    }
-
-    if (getType(obj1) === 'Object') {
-        const keys1 = Object.keys(obj1);
-        const keys2 = Object.keys(obj2);
-        
-        if (!compareObj(keys1, keys2)) return false;
-
-        for (let key of keys1) {
-            if (!compareObj(obj1[key], obj2[key])) return false;
-        }
-
-        return true;
-    }
-
-    return obj1 === obj2;
+    return true;
 }
